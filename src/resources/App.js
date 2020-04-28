65a527a4a942e92d42d2ab95a3121422ce47e6d0
Dmitriy <panqwork@gmail.com>
2020-04-29 00:45:13 +0500

    Added Font Awesome module

--- src/resources/App.js
+++ src/resources/App.js
@@ -4,8 +4,11 @@ import React from 'react';
 import {Header} from './components/header/header';
 import {Navbar} from './components/navbar/navbar';
 import {Content} from './components/content/content';
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {faCheckSquare,faAddressCard} from '@fortawesome/free-solid-svg-icons';
+library.add(faCheckSquare,faAddressCard)
 
 function App() {
   return (
